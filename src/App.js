717d3dd3984ad61779c0fe28760a1b99e2b16f0a
kodhanjo <felix.osambo@student.moringaschool.com>
2021-06-28 13:34:14 +0300

    deploy my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>Welcome to pizza library</h2>
+     <h3>Here is the best place to get your pizza!</h3>
+    </div>
+  );
+}
+
+export default App;
